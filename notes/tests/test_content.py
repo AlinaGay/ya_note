472f92d53a1f157d923c73eb7d22d3d78b66019e
AlinaGay <gayfullina.a@gmail.com>
2025-02-14 14:51:37 +0100

    Add TestAddPage

---
+++ notes/tests/test_content.py
@@ -0,0 +1,67 @@
+from datetime import datetime, timedelta
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from notes.forms import NoteForm
+from notes.models import Note
+
+
+User = get_user_model()
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Пользователь')
+        cls.notes = Note.objects.create(
+            title='Заметка',
+            text='Просто текст.',
+            slug='new',
+            author=cls.author
+        )
+        cls.detail_url = reverse('notes:detail', args=(cls.notes.slug,))
+
+
+class TestListPage(TestCase):
+
+    LIST_URL = reverse('notes:list')
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Пользователь')
+        all_notes = [
+            Note(
+                title=f'Заметка {index}',
+                text='Просто текст.',
+                slug=f'new-{index}',
+                author=cls.author
+            )
+            for index in range(settings.NOTES_COUNT_ON_LIST_PAGE)
+        ]
+        Note.objects.bulk_create(all_notes)
+
+    def test_notes_count(self):
+        self.client.force_login(self.author)
+        response = self.client.get(self.LIST_URL)
+        object_list = response.context['object_list']
+        notes_count = object_list.count()
+        self.assertEqual(notes_count, settings.NOTES_COUNT_ON_LIST_PAGE)
+
+
+class TestAddPage(TestCase):
+
+    ADD_URL = reverse('notes:add')
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Пользователь')
+
+    def test_autorized_user_has_form(self):
+        self.client.force_login(self.author)
+        response = self.client.get(self.ADD_URL)
+        self.assertIn('form', response.context)
+        self.assertIsInstance(response.context['form'], NoteForm)
