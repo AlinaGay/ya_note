243d3424e4016d94c91b070a4065530756da6031
AlinaGay <gayfullina.a@gmail.com>
2025-02-16 00:40:24 +0100

    Add test_anonymous_user_cant_create_note

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -14,13 +14,24 @@ User = get_user_model()
 
 class TestNoteCreation(TestCase):
 
+    NOTE_TITLE = 'Заголовок'
+    NOTE_TEXT = 'Текст'
+    NOTE_SLUG = 'new'
+
     @classmethod
     def setUpTestData(cls):
-        cls.author = User.objects.create(username='Лев Толстой')
-        cls.reader = User.objects.create(username='Читатель простой')
-        cls.note = Note.objects.create(
-            title='Заголовок',
-            text='Текст',
-            slug='new',
-            author=cls.author
-        )
+        cls.url = reverse('notes:add')
+        cls.user = User.objects.create(username='Крокодил')
+        cls.auth_client = Client()
+        cls.auth_client.force_login(cls.user)
+        cls.form_data = {
+            'title': cls.NOTE_TITLE,
+            'text': cls.NOTE_TEXT,
+            'slug': cls.NOTE_SLUG,
+            'author': cls.user
+        }
+
+    def test_anonymous_user_cant_create_note(self):
+        self.client.post(self.url, data=self.form_data)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 0)
