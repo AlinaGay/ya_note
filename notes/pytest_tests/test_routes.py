fb6a0010a9547a26e8d783d4385c7df9f9f6ade1
AlinaGay <gayfullina.a@gmail.com>
2025-02-17 14:37:45 +0100

    Add test_pages_availability_for_auth_user

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -19,3 +19,13 @@ def test_pages_availability_for_anonymous_user(client, name):
     url = reverse(name)
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(admin_client, name):
+    url = reverse(name)
+    response = admin_client.get(url)
+    assert response.status_code == HTTPStatus.OK
