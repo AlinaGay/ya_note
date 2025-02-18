6265733c3190c346525d760c2fad96d83987c747
AlinaGay <gayfullina.a@gmail.com>
2025-02-18 16:25:40 +0100

    Refactor tests

--- notes/pytest_tests/test_content.py
+++ notes/pytest_tests/test_content.py
@@ -1,15 +1,22 @@
+import pytest
 from django.urls import reverse
 
+from notes.forms import NoteForm
 
-def test_note_in_list_for_author(note, author_client):
+
+@pytest.mark.parametrize(
+    'parametrized_client, note_in_list',
+    (
+        (pytest.lazy_fixture('author_client'), True),
+        (pytest.lazy_fixture('not_author_client'), False),
+    )
+)
+def test_notes_list_for_different_users(note, parametrized_client, note_in_list):
     url = reverse('notes:list')
-    response = author_client.get(url)
+    response = parametrized_client.get(url)
     object_list = response.context['object_list']
-    assert note in object_list
+    assert (note in object_list) is note_in_list
 
 
-def test_note_not_in_list_for_other_user(note, not_author_client):
-    url = reverse('notes:list')
-    response = not_author_client.get(url)
-    object_list = response.context['object_list']
-    assert note not in object_list
+
+
