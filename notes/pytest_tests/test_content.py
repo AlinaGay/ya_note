a3e2c60a8f393c361379c4d86681a20081fb2bdc
AlinaGay <gayfullina.a@gmail.com>
2025-02-18 16:11:54 +0100

    Add test_note_not_in_list_for_other_user

--- notes/pytest_tests/test_content.py
+++ notes/pytest_tests/test_content.py
@@ -7,3 +7,9 @@ def test_note_in_list_for_author(note, author_client):
     object_list = response.context['object_list']
     assert note in object_list
 
+
+def test_note_not_in_list_for_other_user(note, not_author_client):
+    url = reverse('notes:list')
+    response = not_author_client.get(url)
+    object_list = response.context['object_list']
+    assert note not in object_list
