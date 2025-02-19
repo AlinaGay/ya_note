6c4bf4977165b3b11b0f217407c036c19f33c1ce
AlinaGay <gayfullina.a@gmail.com>
2025-02-19 09:00:12 +0100

    Add test_other_user_cant_edit_note

--- notes/pytest_tests/test_logic.py
+++ notes/pytest_tests/test_logic.py
@@ -57,4 +57,14 @@ def test_author_can_edit_note(author_client, form_data, note):
     note.refresh_from_db()
     assert note.title == form_data['title']
     assert note.text == form_data['text']
-    assert note.slug == form_data['slug'] 
+    assert note.slug == form_data['slug']
+
+
+def test_other_user_cant_edit_note(not_author_client, form_data, note):
+    url = reverse('notes:edit', args=(note.slug,))
+    response = not_author_client.post(url, form_data)
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    note_from_db = Note.objects.get(id=note.id)
+    assert note.title == note_from_db.title
+    assert note.text == note_from_db.text
+    assert note.slug == note_from_db.slug
