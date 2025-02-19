cd55e1ad5ff1c0aadfa2a6c00c2654d91d4ee988
AlinaGay <gayfullina.a@gmail.com>
2025-02-19 09:04:38 +0100

    Add test_author_can_delete_note

--- notes/pytest_tests/test_logic.py
+++ notes/pytest_tests/test_logic.py
@@ -68,3 +68,10 @@ def test_other_user_cant_edit_note(not_author_client, form_data, note):
     assert note.title == note_from_db.title
     assert note.text == note_from_db.text
     assert note.slug == note_from_db.slug
+
+
+def test_author_can_delete_note(author_client, slug_for_args):
+    url = reverse('notes:delete', args=slug_for_args)
+    response = author_client.post(url)
+    assertRedirects(response, reverse('notes:success'))
+    assert Note.objects.count() == 0
