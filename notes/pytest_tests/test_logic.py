bd222c9e5f75fbd550dd309758bbf98ba7787355
AlinaGay <gayfullina.a@gmail.com>
2025-02-19 07:30:34 +0100

    Add test_logic, test_user_can_create_note

---
+++ notes/pytest_tests/test_logic.py
@@ -0,0 +1,17 @@
+from pytest_django.asserts import assertRedirects
+
+from django.urls import reverse
+
+from notes.models import Note
+
+
+def test_user_can_create_note(author_client, author, form_data):
+    url = reverse('notes:add')
+    response = author_client.post(url, data=form_data)
+    assertRedirects(response, reverse('notes:success'))
+    assert Note.objects.count() == 1
+    new_note = Note.objects.get()
+    assert new_note.title == form_data['title']
+    assert new_note.text == form_data['text']
+    assert new_note.slug == form_data['slug']
+    assert new_note.author == author
